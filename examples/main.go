7c52daa38d6f7c6a78b08d33c91c1795365226da
Ribhararnus Pracutiar <ribhararnus.pracutiar@gmail.com>
2017-01-22 14:12:58 +0700

    Refactor example usage

--- examples/main.go
+++ examples/main.go
@@ -12,26 +12,44 @@ func FooHandler(ctx *queum.Context) {
 	fmt.Println("----------------------")
 	fmt.Println("Details of job", ctx.Job.Name)
 	fmt.Println(ctx.Queue.Data)
+
+	inProgressCount := ctx.Job.QueueCount("in-progress")
+	fmt.Println(inProgressCount)
+
+	progress := ctx.Queue.GetProgress()
+	fmt.Println("Progress", progress)
+	ctx.Queue.SetProgress(progress + 20)
+
+	defer ctx.Continue()
 }
 
 // BarHandler is a handler for bar job
 func BarHandler(ctx *queum.Context) {
 	fmt.Println("What is this", ctx.Job.Name)
+	defer ctx.Done()
 }
 
 func main() {
-	data := map[string]string{
+
+	foo := queum.CreateJob("Foo").
+		Every(5 * time.Second)
+
+	foo.Queue(map[string]string{
 		"test": "baba",
-	}
+	})
+	foo.Queue(map[string]string{
+		"test": "what the fuck",
+	})
+	foo.Queue(map[string]string{
+		"test": "hell yeah",
+	})
 
-	foo := queum.CreateJob("Foo")
-	foo.Every(5 * time.Second)
-	foo.Queue(data)
-	foo.Queue(data)
-	foo.Queue(data)
+	bar := queum.CreateJob("Bar").
+		Every(10 * time.Second)
 
-	bar := queum.CreateJob("Bar")
-	bar.Every(10 * time.Second)
+	bar.Queue(map[string]string{
+		"hello": "world",
+	})
 
 	// Process all job
 	queum.Process("Foo", FooHandler)
